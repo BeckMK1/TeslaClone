920289f374a740a786f7db669f7d6dbc8ff53573
Mikkel Beck Knudsen <47305516+BeckMK1@users.noreply.github.com>
2024-01-05 10:34:10 +0100

    project started

---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <div></div>
+        <div>
+            <slot></slot>
+        </div>
+    </div>
+</template>
+<script setup></script>
+<style lang="scss" scoped></style>
